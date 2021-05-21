7a114f4278334540016b4c719578058512e0b329
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-05-21 15:13:36 -0300

    Creating menu (sidebar)

---
+++ CRM-front/src/app/_components/_shared/menu/menu.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { NbMenuItem } from '@nebular/theme';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  items: NbMenuItem[] = [
+    {
+      title: 'Inicio',
+      icon:'home-outline',
+    },
+    {
+      title: 'Clientes',
+      icon:'people-outline',
+    },
+    {
+      title: 'Funcionarios',
+      icon:'person-outline',
+    },
+    {
+      title: 'Nossos Serviços',
+      icon:'shopping-bag-outline',
+    },
+    {
+      title: 'Contratos',
+      icon: 'file-text-outline',
+      children: [
+        {
+          title: 'Serviços',
+        },
+        {
+          title: 'Planos',
+        },
+      ],
+    },
+    {
+      title: 'Configurações Avançadas',
+      icon: 'settings-outline',
+      children: [
+        {
+          title: 'Setores',
+        },
+        {
+          title: 'Cargos',
+        },
+        {
+          title: 'Nossos Planos',
+        },
+        {
+          title: 'Administradores',
+        },
+      ],
+    },
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
