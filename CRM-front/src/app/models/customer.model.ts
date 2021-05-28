7b855cc61d1439251d11560ca9bbab943f7e279f
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-05-28 17:59:40 -0300

    Update table/Create pages of create and edit/Create Modal Delete

---
+++ CRM-front/src/app/models/customer.model.ts
@@ -0,0 +1,4 @@
+export class Customer{
+    constructor(public id:number, public name:string, public email:string){
+    }
+}
--- CRM-front/src/app/pages/customer/customer.component.ts
+++ CRM-front/src/app/pages/customer/customer.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, EventEmitter, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { NbDialogService } from '@nebular/theme';
+import { Customer } from 'src/app/models/customer.model';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'}
-];
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -22,19 +12,56 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CustomerComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  customers: Customer[] = [
+    new Customer(1,'Gabriel', 'gabriel-feliciano@gmail.com'),
+    new Customer(2,'Lucas', 'lucas-firmiano@gmail.com'),
+    new Customer(3,'Inácio', 'gabriel-inácio@gmail.com'),
+    new Customer(4,'bot', 'server-bot@gmail.com'),
+    new Customer(5,'bot', 'server-bot@gmail.com'),
+    new Customer(6,'bot', 'server-bot@gmail.com'),
+    new Customer(7,'bot', 'server-bot@gmail.com'),
+    new Customer(8,'bot', 'server-bot@gmail.com'),
+  ];
+  
+  displayedColumns: string[] = ['id', 'nome', 'email','actions'];
+  dataSource: MatTableDataSource<Customer>;
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
 
   ngOnInit(): void {
   }
-  constructor() {
+
+  constructor(private dialogService: NbDialogService) {
+    this.dataSource = new MatTableDataSource(this.customers);
+  }
+  //INICIAR PÁGINAÇÃO E QTD DE ITENS
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  //FILTRO
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  open(dialog: TemplateRef<any>) {
+    this.dialogService.open(dialog);
+  }
+
+  onSelected(){
+
   }
 }
+
 
   
 
