b90949139b3164883922cc8a6d8efacc53524f78
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-06-01 17:54:46 -0300

    New modals address and customer

---
+++ CRM-front/src/app/models/address.model.ts
@@ -0,0 +1,12 @@
+export class Address{
+    constructor(
+        public cep: string,
+        public logradouro: string,
+        public bairro: string,
+        public UF: string,
+        public numero: string,
+        public complemento: string,       
+    ){
+
+    }
+} 
