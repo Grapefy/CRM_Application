7299c36136504ae0bc55dd32453b7b6e8132e8b4
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-06-24 17:38:13 -0300

    Modal Forget Password

--- CRM-front/src/app/pages/login/login.component.ts
+++ CRM-front/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogService: NbDialogService) { }
 
   ngOnInit(): void {
   }
@@ -16,6 +17,10 @@ export class LoginComponent implements OnInit {
 
   toggle(checked: boolean) {
     this.checked = checked;
+  }
+
+  openForgetPassword(dialog: TemplateRef<any>) {
+    this.dialogService.open(dialog);
   }
 
 }
